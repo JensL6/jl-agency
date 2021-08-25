827dd38f1abf501d1cc6cf5846cba5be0fae8d16
Mxnsterr <freezz.eu@gmail.com>
2021-08-25 17:17:09 +0200

    index and artist page

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,42 +5,60 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header/Header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `auto`,
-          padding: 0,
-        }}
-      >
-        <main>{children}</main>
-      </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+ import React, { useState } from "react"
+ import PropTypes from "prop-types"
+ import { useStaticQuery, graphql } from "gatsby"
+ 
+ import Header from "./header/Header"
+ import Hamburger from "./header/Hamburger"
+ import OverlayMenu from "./header/OverlayMenu"
+ import "./layout.css"
+ 
+ const Layout = ({ children }) => {
+   const [menuOpen, setMenuOpen] = useState(false)
+ 
+   const handleOverlayMenu = () => setMenuOpen(!menuOpen)
+ 
+   const data = useStaticQuery(graphql`
+     query SiteTitleQuery {
+       site {
+         siteMetadata {
+           title
+         }
+       }
+     }
+   `)
+ 
+   return (
+     <>
+       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+       <Hamburger handleOverlayMenu={handleOverlayMenu} />
+       <OverlayMenu handleOverlayMenu={handleOverlayMenu} menuOpen={menuOpen} />
+       <div
+         style={{
+           margin: `0 auto`,
+           maxWidth: "auto",
+           padding: 0,
+           paddingBottom: "2rem",
+         }}
+       >
+         <main>{children}</main>
+         {/* <footer
+           style={{
+             marginTop: `2rem`,
+           }}
+         >
+           © {new Date().getFullYear()}, Built with
+           {` `}
+           <a href="https://www.gatsbyjs.com">Gatsby</a>
+         </footer> */}
+       </div>
+     </>
+   )
+ }
+ 
+ Layout.propTypes = {
+   children: PropTypes.node.isRequired,
+ }
+ 
+ export default Layout
